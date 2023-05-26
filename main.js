6d3a5a1ddad80fe464984547cfa5611239ce2907
Andrey_Davidovich <andrejdavidovich88@gmail.com>
2023-05-26 22:05:11 +0300

    created functions

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // Функция palindrome
-const polindrome = function (str) {
+const myFun = str => {
   let word = '';
   let revWord = '';
   let end = str.length - 1;
 
-  for (let i = 0; i < str.length; i++ ) {
+  for (let i = 0; i < str.length; i++) {
     word += str[i];
     revWord += str[end];
     end -= 1;
@@ -28,27 +28,71 @@ const max = function (a, b) {
   return b;
 };
 
-
 // Функция min(a, b)
 // попробуйте переписать функцию, используя тернарный оператор.
 const min1 = function (a, b) {
-  return (a < b ? a : b);
+  return a < b ? a : b;
 };
 
 // функция max(a,b)
 // попробуйте переписать функцию, используя тернарный оператор.
 const max1 = function (a, b) {
-  return (a > b ? a : b);
+  return a > b ? a : b;
 };
 
 let value = '3334'; // Таким образом добился чтобы и цифры тоже приводил к строке и сравнивал, но не уверен что так правильно))
 value = String(value);
+const num1 = 20;
+const num2 = 50;
+
+console.log(myFun(value));
+console.log(min(num1, num2));
+console.log(max(num1, num2));
+console.log(min1(num1, num2));
+console.log(max1(num1, num2));
 
 
-console.log(polindrome(value));
-console.log(min(20,15));
-console.log(max(50,30));
-console.log(min1(20,15));
-console.log(max1(50,30));
+// Замена элементов массива:
+//   создайте массив с десятью случайными элементами от 0 до 100;
+// напишите функцию, которая будет заменять все 0 на строку 'zero';
+// выведите полученный массив в консоль (пример: [12, 53, '2zero', 18, 22, '1zerozero', 43, 57, '5zero', 1]).
+const funList = function (...args) {
+  const zero = 'zero';
+  let newList = [];
+
+  for (let i = 0; i < args.length; i++) {
+    let word = String(args[i]);
+
+    for (let i = 0; i < word.length; i++) {
+      if (word[i] === '0') {
+        word[i] = 'zero';
+        word = word + zero;
+      }
+    }
+    newList += word + ' ';
+  }
+  return newList;
+};
 
 
+console.log(funList(1, 3, 40, 51, 30, 25, 100, 0, 16, 50));
+
+// const funList = function (...args) {
+//   let list = [];
+//   for (let i = 0; i < args.length; i++) {
+//     let word = String(args[i]);
+//     let newWord = '';
+//
+//     for (let i = 0; i < word.length; i++) {
+//       if (word[i] !== '0') {
+//         newWord[i] = word[i];
+//       }
+//       newWord[i] = 'zero';
+//     }
+//
+//     list += newWord;
+//   }
+//   return list;
+// };
+//
+// console.log(funList(1, 3, 40, 51, 30, 25, 100, 0, 16, 50));
