6f1c31fe94a27a359d5fdde869f2ee0b1bdeed61
Andrey_Davidovich <andrejdavidovich88@gmail.com>
2023-05-29 16:42:56 +0300

    created function getList

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ const max1 = (a, b) => {
 // Замена элементов массива:
 // создайте массив с десятью случайными элементами от 0 до 100;
 
-const getRandomList = (min, max) =>{
+const getRandomList = (min, max) => {
   const list = [];
   for (let i = 0; i < 10; i++) {
     const numRandom = Math.floor(Math.random() * (max - min)) + min;
@@ -55,64 +55,32 @@ const getRandomList = (min, max) =>{
 
 // напишите функцию, которая будет заменять все 0 на строку 'zero';
 // выведите полученный массив в консоль (пример: [12, 53, '2zero', 18, 22, '1zerozero', 43, 57, '5zero', 1]).
+const getList = () => {
+  for (let i = 0; i < array.length; i++) {
+    const value = `${array[i]}`;
+    let newValue = '';
+
+    for (let j = 0; j < value.length; j++) {
+      if (value[j] === '0') {
+        newValue += 'zero';
+      } else {
+        newValue += value[j];
+      }
+    }
+    array[i] = newValue;
+  }
+  return array;
+};
 
 let value = '3334'; // Таким образом добился чтобы и цифры тоже приводил к строке и сравнивал, но не уверен что так правильно))
 value = String(value);
 const num1 = 20;
 const num2 = 50;
-
+const array = getRandomList(0, 101);
 console.log(myFun(value));
 console.log(min(num1, num2));
 console.log(max(num1, num2));
 console.log(min1(num1, num2));
 console.log(max1(num1, num2));
-console.log(getRandomList(0, 101));
-
-
-
-
-
-
-
-
-
-// const funList = function (...args) {
-//   console.log(args);
-//   const zero = 'zero';
-//   let newList = [];
-//
-//   for (let i = 0; i < args.length; i++) {
-//     let word = String(args[i]);
-//
-//     for (let i = 0; i < word.length; i++) {
-//       if (word[i] === '0') {
-//         word[i] = 'zero';
-//         word = word + zero;
-//       }
-//     }
-//     newList += word + ' ';
-//   }
-//   return newList;
-// };
-//
-// console.log(funList(1, 3, 40, 51, 30, 25, 100, 0, 16, 50));
-
-// const funList = function (...args) {
-//   let list = [];
-//   for (let i = 0; i < args.length; i++) {
-//     let word = String(args[i]);
-//     let newWord = '';
-//
-//     for (let i = 0; i < word.length; i++) {
-//       if (word[i] !== '0') {
-//         newWord[i] = word[i];
-//       }
-//       newWord[i] = 'zero';
-//     }
-//
-//     list += newWord;
-//   }
-//   return list;
-// };
-//
-// console.log(funList(1, 3, 40, 51, 30, 25, 100, 0, 16, 50));
+console.log(array);
+console.log(getList());
