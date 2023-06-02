80bde9a2cef73abefc6b400c2dc2b99b6c36b4b9
Andrey_Davidovich <andrejdavidovich88@gmail.com>
2023-06-02 13:50:21 +0300

    Created function getColors

--- src/scripts/lesson4.js
+++ src/scripts/lesson4.js
@@ -23,31 +23,32 @@ export const num2 = 12;
 // по первому нажатию на абзац он должен покраситься в первый цвет из массива, по второму нажатию - во второй и так далее;
 // цвета из массива меняются бесконечно;
 // все абзацы работают независимо.
+export const getColors = () => {
+  let color = '';
+  let count = 0;
+  return (array) => {
+    for (let i = 1; i < array.length; i++) {
+      if (count <= i) {
+        color = array[count];
+        count++;
+        return color;
+      } else if (count === array.length) {
+        count = 0;
+      }
+    }
+  };
+};
+
 export const text1 = document.getElementById('text1');
 export const text2 = document.getElementById('text2');
 export const text3 = document.getElementById('text3');
-
-export const colorState = {
-  text1: null,
-  text2: null,
-  text3: null,
-};
-export let count = 0;
-export const getColors = (array) => {
-
-  for ( let i = 1; i < array.length; i++) {
-    if (count <= i) {
-      colorState.text1 = array[count];
-      count++;
-      return colorState.text1;
-    } else if (count === array.length) {
-      count = 0;
-    }
-  }
-};
-
-export const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
-
-
-
-
+export const colors = [
+  'magenta',
+  'cyan',
+  'firebrick',
+  'springgreen',
+  'skyblue',
+];
+export const click1 = getColors();
+export const click2 = getColors();
+export const click3 = getColors();
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,6 @@
 import * as lesson3 from './lesson3.js';
 import * as lesson4 from './lesson4.js';
-import {colors, getColors, text1, text2, text3} from './lesson4.js';
-
-
-
+import {click1, click2, click3, colors, text1, text2, text3} from './lesson4.js';
 
 console.log('--------------lesson-3-----------------');
 console.log(lesson3.myFun(lesson3.value));
@@ -17,14 +14,13 @@ console.log('--------------lesson-4-----------------');
 console.log(lesson4.sum(lesson4.num1)(lesson4.num2));
 
 text1.addEventListener('click', (event) => {
-  event.target.style.color = getColors(colors);
+  event.target.style.color = click1(colors);
 });
 
 text2.addEventListener('click', (event) => {
-  event.target.style.color = getColors(colors);
+  event.target.style.color = click2(colors);
 });
 
 text3.addEventListener('click', (event) => {
-  event.target.style.color = getColors(colors);
+  event.target.style.color = click3(colors);
 });
-
