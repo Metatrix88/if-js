e63850111327cf2968f7bfe472dbbd0a5fae5a19
Andrey_Davidovich <andrejdavidovich88@gmail.com>
2023-06-06 13:52:51 +0300

    Created function getDate in file lesson5.js - format text

--- tests/lesson5.test.js
+++ tests/lesson5.test.js
@@ -1,6 +1,5 @@
 import { getDate } from '../src/scripts/lesson5';
 
-
 describe('Ввод даты формат: гггг-мм-дд', () => {
   test('должна вернуться дата формата дд.мм.гггг', () => {
     expect(getDate('2020-13-24')).toBe('24.13.2020');
