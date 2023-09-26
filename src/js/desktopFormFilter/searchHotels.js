183b18f267b15b40902eae901d7e6ea34df0eb99
Andrey_Davidovich <andrejdavidovich88@gmail.com>
2023-09-26 16:19:55 +0300

    fix commit

--- src/js/desktopFormFilter/searchHotels.js
+++ src/js/desktopFormFilter/searchHotels.js
@@ -7,9 +7,7 @@ import {
 import { getPopularHotels } from '../constants-utils/helpers.js';
 import { urlAllHotels } from '../constants-utils/constants.js';
 
-// core version + navigation, pagination modules:
-import Swiper from '../../../node_modules/swiper/swiper-bundle.min.mjs';
-import { Navigation, Pagination } from '../../../node_modules/swiper/modules';
+import { getSwiper } from '../constants-utils/swiper.js';
 
 export const searchHotels = async (e) => {
   e.preventDefault();
@@ -21,25 +19,5 @@ export const searchHotels = async (e) => {
   await createSectionAvailable();
   await addHotelsAvailable(hotels);
 
-  const swiper = new Swiper('.swiper', {
-    modules: [Navigation, Pagination],
-    effect: 'coverflow',
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: 'auto',
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true,
-    },
-    pagination: {
-      el: '.swiper-pagination',
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+  getSwiper();
 };
