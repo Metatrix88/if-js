96865f328828f13778db3dbbb45f5f585dd1923f
Andrey_Davidovich <andrejdavidovich88@gmail.com>
2023-09-26 17:18:37 +0300

    fix swiper

--- src/js/constants-utils/swiper.js
+++ src/js/constants-utils/swiper.js
@@ -1,6 +1,7 @@
 // core version + navigation, pagination modules:
-import Swiper from '../../../node_modules/swiper/swiper-bundle.min.mjs';
+//import Swiper from '../../../node_modules/swiper/swiper-bundle.min.mjs';
 import { Navigation, Pagination } from '../../../node_modules/swiper/modules';
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs'
 
 export const getSwiper = () => {
   return new Swiper('.swiper', {
