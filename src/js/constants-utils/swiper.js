305d24f281ae5c58763fc47dff34e27d6688b08d
Andrey_Davidovich <andrejdavidovich88@gmail.com>
2023-09-27 16:50:35 +0300

    swiper fix section avainable

--- src/js/constants-utils/swiper.js
+++ src/js/constants-utils/swiper.js
@@ -1,36 +1,19 @@
-// import Swiper from '../libraries/swiper/swiper-bundle.min.mjs';
-// import { Navigation, Pagination } from '../libraries/swiper/modules';
-
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs';
 
-// export const getSwiperCoverflow = () => {
-//   return new Swiper('.swiper__coverflow', {
-//     effect: 'coverflow',
-//     grabCursor: true,
-//     centeredSlides: true,
-//     slidesPerView: 'auto',
-//     slidesPerGroup: 1,
-//     coverflowEffect: {
-//       rotate: 50,
-//       stretch: 0,
-//       depth: 100,
-//       modifier: 1,
-//       slideShadows: true,
-//     },
-//     pagination: {
-//       el: '.swiper-pagination',
-//     },
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-//   });
-// };
-
 export const getSwiperCoverflow = () => {
-  return new Swiper('.swiper', {
-    slidesPerView: 4,
-    spaceBetween: 1,
+  return new Swiper('.swiper__coverflow', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    slidesPerGroup: 1,
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
